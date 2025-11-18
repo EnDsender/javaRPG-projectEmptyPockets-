7e1f573c34ef0765f5d659b7f7dd43dceea49352
dossanic <dossantosnicholas99@gmail.com>
2025-11-18 12:41:53 -0500

    add spring boot project to repo

---
+++ RPGSpringBoot/src/main/java/ca/sheridancollege/dossanic/RpgSpringBootApplication.java
@@ -0,0 +1,13 @@
+package ca.sheridancollege.dossanic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpgSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpgSpringBootApplication.class, args);
+	}
+
+}
